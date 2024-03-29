df1cdd00c98476ab3f57b0d18db577f1f34491c3
Jeremy Soller <jackpot51@gmail.com>
2024-03-29 09:47:26 -0600

    Fix build on i686

--- src/platform_impl/orbital/event_loop.rs
+++ src/platform_impl/orbital/event_loop.rs
@@ -599,7 +599,7 @@ impl<T: 'static> EventLoop<T> {
 
                 if let Some(duration) = instant.checked_duration_since(start) {
                     time.tv_sec += duration.as_secs() as i64;
-                    time.tv_nsec += duration.subsec_nanos() as i64;
+                    time.tv_nsec += duration.subsec_nanos() as libc::c_long;
                     // Normalize timespec so tv_nsec is not greater than one second.
                     while time.tv_nsec >= 1_000_000_000 {
                         time.tv_sec += 1;
